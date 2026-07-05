dbb7972aae3eabcb3f9795ab26c1df108aeefa3a
Jumandi Andrade <dark.vilma@gmail.com>
2026-07-05 14:05:01 -0500

    chore: bootstrap Spring Boot project (Java 21, Gradle, Web/JPA/H2/Validation/Lombok)

---
+++ src/main/java/org/ups/facturacionfirmas/FacturacionFirmasServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ups.facturacionfirmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionFirmasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionFirmasServiceApplication.class, args);
+	}
+
+}
